b3cd6292652650830b03efc7d0d38d083aea8d73
Yurii Tyshchenko <orty222@gmail.com>
2023-03-18 10:38:55 +0100

    Improved animations

--- src/App.tsx
+++ src/App.tsx
@@ -13,6 +13,7 @@ import {
 
 import { MdVerified } from "react-icons/md";
 import Block from "./components/Block";
+import { useState } from "react";
 
 const now = new Date().toLocaleTimeString("en-US", {
   hour: "numeric",
@@ -20,86 +21,121 @@ const now = new Date().toLocaleTimeString("en-US", {
 });
 
 function App() {
+  const [openSettings, setOpenSettings] = useState(false);
+
   return (
     <div className="w-screen h-screen flex overflow-hidden">
       {/* Left side */}
-      <div className="w-1/4 h-full py-0.5 px-2 bg-zinc-700 relative">
-        <div className="w-full py-2 border-b-2 border-zinc-800">
-          <UserBlock>
-            <UserBlock.Avatar name="Gravity Impulse" />
-            <UserBlock.Body>
-              <UserBlock.Content>
-                <span>Gravity Impulse</span>
-              </UserBlock.Content>
-              <UserBlock.SubContent>
-                <span>@gavityimpulse</span>
-              </UserBlock.SubContent>
-            </UserBlock.Body>
-            <UserBlock.Tail>
-              <Block.Icon>
-                <CiMenuBurger />
-              </Block.Icon>
-            </UserBlock.Tail>
-          </UserBlock>
-        </div>
-        {/* Friends/Groups Section */}
-        <div className="w-full h-full py-1 overflow-y-auto">
-          <UserBlock animate hover>
-            <UserBlock.Avatar name="Friend XD">
-              <div className="w-3 h-3 right-0.5 top-0.5 border-2 rounded-full bg-sky-500 border-zinc-800 absolute"></div>
-            </UserBlock.Avatar>
-            <UserBlock.Body>
-              <UserBlock.Content>
-                <span>Friend XD</span>
-                <CiVolumeMute className="text-slate-400" />
-              </UserBlock.Content>
-              <UserBlock.SubContent>
-                What's up bro?dsadsadaddaasddsadsadadsdsa
-              </UserBlock.SubContent>
-            </UserBlock.Body>
-            <UserBlock.Tail>
-              <div className="min-w-[5rem] h-12 flex flex-col items-end">
-                <p className="text-gray-500 text-xs leading-7">{now}</p>
-                <p className="text-sky-500 text-xs leading-5">Seen</p>
-              </div>
-            </UserBlock.Tail>
-          </UserBlock>
-
-          <UserBlock animate hover>
-            <UserBlock.Avatar name="Friend XD">
-              <MdVerified className="w-4 h-4 absolute right-0.5 top-0.5 fill-green-500 rounded-full bg-zinc-800 border-2 border-zinc-800" />
-            </UserBlock.Avatar>
-            <UserBlock.Body>
-              <UserBlock.Content>
-                <span>Friend XD</span>
-                <CiVolumeMute className="text-slate-400" />
-              </UserBlock.Content>
-              <UserBlock.SubContent>
-                What's up bro?dsadsadaddaasddsadsadadsdsa
-              </UserBlock.SubContent>
-            </UserBlock.Body>
-            <UserBlock.Tail>
-              <div className="min-w-[5rem] h-12 flex flex-col items-end">
-                <p className="text-gray-500 text-xs leading-7">{now}</p>
-                <p className="text-sky-500 text-xs leading-5">Seen</p>
-              </div>
-            </UserBlock.Tail>
-          </UserBlock>
-          {/* End of chats */}
-        </div>
-
-        <Block
-          onClick={() => console.log("hello world")}
-          absolute="left-0 bottom-0"
+      <div className={`w-1/4 h-full py-0.5 px-2 bg-zinc-700 relative`}>
+        <div
+          className={openSettings ? `animate-dissapear` : "animate-appear"}
+          onAnimationEnd={(e) => {
+            if (e.elapsedTime === 0.5) {
+              e.currentTarget.classList.add("hidden");
+              e.currentTarget.classList.remove("animate-dissapear");
+            } else {
+              console.log("test");
+              e.currentTarget.classList.remove("hidden", "animate-appear");
+            }
+          }}
         >
-          <Block.Icon>
-            <CiSettings />
-          </Block.Icon>
-          <Block.Content>Settings</Block.Content>
-          <Block.Icon>
-            <CiCircleChevUp />
-          </Block.Icon>
-        </Block>
+          <div className="w-full py-2 border-b-2 border-zinc-800">
+            <UserBlock>
+              <UserBlock.Avatar name="Gravity Impulse" />
+              <UserBlock.Body>
+                <UserBlock.Content>
+                  <span>Gravity Impulse</span>
+                </UserBlock.Content>
+                <UserBlock.SubContent>
+                  <span>@gavityimpulse</span>
+                </UserBlock.SubContent>
+              </UserBlock.Body>
+              <UserBlock.Tail>
+                <Block.Icon>
+                  <CiMenuBurger />
+                </Block.Icon>
+              </UserBlock.Tail>
+            </UserBlock>
+          </div>
+          {/* Friends/Groups Section */}
+          <div className="w-full h-full py-1 overflow-y-auto">
+            <UserBlock animate hover>
+              <UserBlock.Avatar name="Friend XD">
+                <div className="w-3 h-3 right-0.5 top-0.5 border-2 rounded-full bg-sky-500 border-zinc-800 absolute"></div>
+              </UserBlock.Avatar>
+              <UserBlock.Body>
+                <UserBlock.Content>
+                  <span>Friend XD</span>
+                  <CiVolumeMute className="text-slate-400" />
+                </UserBlock.Content>
+                <UserBlock.SubContent>
+                  What's up bro?dsadsadaddaasddsadsadadsdsa
+                </UserBlock.SubContent>
+              </UserBlock.Body>
+              <UserBlock.Tail>
+                <div className="min-w-[5rem] h-12 flex flex-col items-end">
+                  <p className="text-gray-500 text-xs leading-7">{now}</p>
+                  <p className="text-sky-500 text-xs leading-5">Seen</p>
+                </div>
+              </UserBlock.Tail>
+            </UserBlock>
+
+            <UserBlock animate hover>
+              <UserBlock.Avatar name="Friend XD">
+                <MdVerified className="w-4 h-4 absolute right-0.5 top-0.5 fill-green-500 rounded-full bg-zinc-800 border-2 border-zinc-800" />
+              </UserBlock.Avatar>
+              <UserBlock.Body>
+                <UserBlock.Content>
+                  <span>Friend XD</span>
+                  <CiVolumeMute className="text-slate-400" />
+                </UserBlock.Content>
+                <UserBlock.SubContent>
+                  What's up bro?dsadsadaddaasddsadsadadsdsa
+                </UserBlock.SubContent>
+              </UserBlock.Body>
+              <UserBlock.Tail>
+                <div className="min-w-[5rem] h-12 flex flex-col items-end">
+                  <p className="text-gray-500 text-xs leading-7">{now}</p>
+                  <p className="text-sky-500 text-xs leading-5">Seen</p>
+                </div>
+              </UserBlock.Tail>
+            </UserBlock>
+            {/* End of chats */}
+          </div>
+        </div>
+        <div
+          id="settings"
+          className="w-full transition-all"
+          onAnimationEnd={(e) => {
+            if (openSettings) {
+              e.currentTarget.classList.remove("animate-move-in", "absolute");
+            } else {
+              e.currentTarget.classList.remove("absolute", "animate-move-out");
+            }
+          }}
+        >
+          <Block
+            onClick={() => {
+              const set = document.getElementById("settings");
+              if (openSettings) {
+                set?.classList.add("absolute", "animate-move-out");
+              } else {
+                set?.classList.add("absolute", "left-0", "animate-move-in");
+              }
+              setOpenSettings(!openSettings);
+            }}
+            absolute={!openSettings ? "left-0 bottom-0" : undefined}
+          >
+            <Block.Icon>
+              <CiSettings />
+            </Block.Icon>
+            <Block.Content>Settings</Block.Content>
+            <Block.Icon>
+              {openSettings ? <CiCircleChevDown /> : <CiCircleChevUp />}
+            </Block.Icon>
+          </Block>
+          {openSettings ? <></> : null}
+        </div>
       </div>
       {/* End of Left Side */}
 
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -3,8 +3,15 @@ module.exports = {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
     extend: {
+      animationDirection: {
+        "normal-reverse": "normal, reverse",
+      },
       animation: {
         wiggle: "wiggle 1.8s linear infinite",
+        dissapear: "dissapear .5s linear",
+        appear: "dissapear .9s linear reverse",
+        "move-in": "move .8s linear",
+        "move-out": "move .5s linear reverse",
       },
       keyframes: {
         wiggle: {
@@ -14,6 +21,15 @@ module.exports = {
           "36%": { transform: "rotate(-20deg)" },
           "50%": { transform: "rotate(0deg)" },
           "100%": { transform: "rotate(0deg)" },
+        },
+        dissapear: {
+          "0%": { opacity: "100%" },
+          "80%": { opacity: "20%", scale: "100%" },
+          "100%": { opacity: "0%", scale: "80%", display: "none" },
+        },
+        move: {
+          "0%": { top: "90%" },
+          "100%": { top: "0%" },
         },
       },
     },
